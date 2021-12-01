77fb20e2c856fdb8cbd9f8d347e7d3c985395a97
mkrtashot <mkrtchyan.ashot@yahoo.com>
2021-12-01 13:51:33 +0400

    Added Working with variables JS file + edited html file

--- JAVASCRIPT INFO TASKS.js
+++ JAVASCRIPT INFO TASKS.js
@@ -22,8 +22,10 @@ let user = "Current Visitor's Name";
 // Uppercase const?
 
 // Examine the following code:
-const birthday = '18.04.1982';
-const age = someCode(birthday);
+
+// const birthday = '18.04.1982';
+// const age = someCode(birthday);
+
 // Here we have a constant birthday date and the age is calculated from birthday with the help of some code (it is not provided for shortness, and because details don’t matter here).
 
 // Would it be right to use upper case for birthday? For age? Or even for both?
---
+++ Working with variables.js
@@ -0,0 +1,11 @@
+// Կոնկրետ ստեղ admin1 ու name1 է օգտագործված, որովհետև նույն փոփոխականները հայտարարվել են javascript info tasks.js ֆալյում
+
+// Declare two variables: admin and name.
+let admin1;
+let name1;
+// Write the string "John" to the name variable.
+name1 = "John";
+// Copy the value from the name variable to admin.
+admin1 = name1;
+// Display the value admin.
+console.log(admin1)
